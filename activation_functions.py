7cd5342b82ab6c3c3bba854e18cb183e7dcfe7e9
alessandroquirile <a.quirile@studenti.unina.it>
2022-09-11 21:10:04 +0200

    typos correction

--- activation_functions.py
+++ activation_functions.py
@@ -1,26 +1,26 @@
 import numpy as np
 
 
-def sigmoid(a, derivate=False):
+def sigmoid(a, derivative=False):
     f = 1 / (1 + np.exp(-a))
     df = f * (1 - f)
-    if derivate:
+    if derivative:
         return df
     return f
 
 
-def identity(a, derivate=False):
+def identity(a, derivative=False):
     f = a
     df = 1
-    if derivate:
+    if derivative:
         return df
     return f
 
 
-def relu(a, derivate=False):
+def relu(a, derivative=False):
     f = a if a > 0 else 0
     df = 1 if a > 0 else 0 if a < 0 else None
-    if derivate:
+    if derivative:
         return df
     return f
 
