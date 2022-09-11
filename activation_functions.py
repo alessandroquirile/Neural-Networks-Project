6a478e43f0c3a07a6467d5bc1c25f92252deb193
alessandroquirile <a.quirile@studenti.unina.it>
2022-09-11 20:30:54 +0200

    added derivatives to activation functions

--- activation_functions.py
+++ activation_functions.py
@@ -1,13 +1,26 @@
 import numpy as np
 
 
-def sigmoid(a):
-    return 1 / (1 + np.exp(-a))
+def sigmoid(a, derivate=False):
+    f = 1 / (1 + np.exp(-a))
+    df = f * (1 - f)
+    if derivate:
+        return df
+    return f
 
 
-def identity(a):
-    return a
+def identity(a, derivate=False):
+    f = a
+    df = 1
+    if derivate:
+        return df
+    return f
 
 
-def relu(a):
-    return a if a > 0 else 0
+def relu(a, derivate=False):
+    f = a if a > 0 else 0
+    df = 1 if a > 0 else 0 if a < 0 else None
+    if derivate:
+        return df
+    return f
+
