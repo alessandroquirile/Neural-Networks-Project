7082650e4e7b503c343eafeef4feb673e23b1938
alessandroquirile <a.quirile@studenti.unina.it>
2022-09-15 11:00:01 +0200

    bug fixing

--- activation_functions.py
+++ activation_functions.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 def sigmoid(a, derivative=False):
-    f = 1 / (1 + np.exp(-a))
-    df = f * (1 - f)
+    f_a = 1 / (1 + np.exp(-a))
+    df_a = np.multiply(f_a, (1 - f_a))  # element-wise
     if derivative:
-        return df
-    return f
+        return df_a
+    return f_a
 
 
 def identity(a, derivative=False):
@@ -23,4 +23,3 @@ def relu(a, derivative=False):
     if derivative:
         return df
     return f
-
