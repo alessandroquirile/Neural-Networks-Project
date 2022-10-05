025dec508f48c872b6d7fb5c3d7128cb4e462e51
alessandroquirile <a.quirile@studenti.unina.it>
2022-10-05 10:03:06 +0200

    cross-entropy and softmax are now more numerically stable

--- loss_functions.py
+++ loss_functions.py
@@ -1,4 +1,5 @@
 from post_processing_functions import *
+from scipy.special import xlog1py
 
 
 def sum_of_squares(y, t, derivative=False):
@@ -11,8 +12,18 @@ def cross_entropy(y, t, derivative=False, post_process=False):
     if post_process:
         if derivative:
             return y - t
-        return -np.sum(np.sum(np.multiply(t, np.log(softmax(y))), axis=0))  # axis=0 somma di elementi per colonne
+        return -np.sum(np.sum(xlog1py(t, softmax(y)), axis=0))  # supposed to be more numerically stable
     else:
         if derivative:
             return -np.sum(np.divide(t, y), axis=1)  # axis=1 somma di elementi per righe
-        return -np.sum(np.sum(np.multiply(t, np.log(y)), axis=0))
+        return -np.sum(np.sum(xlog1py(t, y), axis=0))
+
+    # epsilon = 10 ** -5  # for avodiing log(0)
+    # if post_process:
+    #     if derivative:
+    #         return y - t
+    #     return -np.sum(np.sum(np.multiply(t, np.log(epsilon + softmax(y))), axis=0))  # axis=0 somma di elementi per colonne
+    # else:
+    #     if derivative:
+    #         return -np.sum(np.divide(t, y), axis=1)  # axis=1 somma di elementi per righe
+    #     return -np.sum(np.sum(np.multiply(t, np.log(y)), axis=0))
--- post_processing_functions.py
+++ post_processing_functions.py
@@ -2,4 +2,6 @@ import numpy as np
 
 
 def softmax(y):
-    return np.exp(y) / np.sum(np.exp(y))
+    e_x = np.exp(y - np.max(y))
+    return e_x / e_x.sum()
+    # return np.exp(y) / np.sum(np.exp(y))  # may cause over-under flow
