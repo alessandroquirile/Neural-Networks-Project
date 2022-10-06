bf736e6a51d562634ab84c59346da805cd00b293
alessandroquirile <a.quirile@studenti.unina.it>
2022-10-06 20:38:11 +0200

    derivatives are now computed within if body

--- activation_functions.py
+++ activation_functions.py
@@ -4,23 +4,20 @@ from scipy.special import expit
 def sigmoid(a, derivative=False):
     # f_a = 1 / (1 + np.exp(-a))  # potrebbe causare overflow
     f_a = expit(-a)
-    df_a = np.multiply(f_a, (1 - f_a))  # element-wise
     if derivative:
-        return df_a
+        return np.multiply(f_a, (1 - f_a))
     return f_a
 
 
 def identity(a, derivative=False):
     f_a = a
-    df_a = np.ones(np.shape(a))
     if derivative:
-        return df_a
+        return np.ones(np.shape(a))
     return f_a
 
 
 def relu(a, derivative=False):
     f_a = np.maximum(0, a)
-    df_a = (a > 0) * 1
     if derivative:
-        return df_a
+        return (a > 0) * 1
     return f_a
