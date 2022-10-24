a9d4a7c88d76d9e0911add4265b331ed0145135f
alessandroquirile <a.quirile@studenti.unina.it>
2022-10-24 13:15:34 +0200

    is_balanced method refactoring

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import math
+from collections import Counter
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -53,44 +54,6 @@ def balanced(targets_train, targets_val, targets_test):
 
 # https://www.researchgate.net/figure/Class-percentages-in-MNIST-dataset_fig2_320761896
 def is_balanced(targets):
-    zeros = ones = twos = threes = fours = fives = sixes = sevens = eights = nines = 0
-    total_targets = np.shape(targets)[0]
-    for target in np.nditer(targets):
-        if target == 0:
-            zeros += 1
-        elif target == 1:
-            ones += 1
-        elif target == 2:
-            twos += 1
-        elif target == 3:
-            threes += 1
-        elif target == 4:
-            fours += 1
-        elif target == 5:
-            fives += 1
-        elif target == 6:
-            sixes += 1
-        elif target == 7:
-            sevens += 1
-        elif target == 8:
-            eights += 1
-        elif target == 9:
-            nines += 1
-    """print("Class percentage:")
-    print(f"Zeros {zeros / total_targets * 100}, ones {ones / total_targets * 100}, "
-          f"twos {twos / total_targets * 100}, threes {threes / total_targets * 100}, "
-          f"fours {fours / total_targets * 100}, fives {fives / total_targets * 100}, "
-          f"sixes {sixes / total_targets * 100}, sevens {sevens / total_targets * 100}, "
-          f"eights {eights / total_targets * 100}, nines {nines / total_targets * 100}")"""
-
-    abs_tol = 2.0
-    if math.isclose(zeros / total_targets * 100, ones / total_targets * 100, abs_tol=abs_tol) and \
-            math.isclose(ones / total_targets * 100, twos / total_targets * 100, abs_tol=abs_tol) and \
-            math.isclose(twos / total_targets * 100, threes / total_targets * 100, abs_tol=abs_tol) and \
-            math.isclose(threes / total_targets * 100, fours / total_targets * 100, abs_tol=abs_tol) and \
-            math.isclose(fours / total_targets * 100, fives / total_targets * 100, abs_tol=abs_tol) and \
-            math.isclose(fives / total_targets * 100, sixes / total_targets * 100, abs_tol=abs_tol) and \
-            math.isclose(sevens / total_targets * 100, eights / total_targets * 100, abs_tol=abs_tol) and \
-            math.isclose(eights / total_targets * 100, nines / total_targets * 100, abs_tol=abs_tol):
-        return True
-    return False
+    c = Counter(targets)
+    percentages = [v / c.total() * 100 for v in c.values()]
+    return math.isclose(min(percentages), max(percentages), abs_tol=2.5)
